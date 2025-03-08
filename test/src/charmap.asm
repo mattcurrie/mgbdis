98ff174ed1e3b766e1f96645cabc43f84c2d67bc
Matt Currie <me@mattcurrie.com>
2025-03-08 13:20:04 +1300

    Add test

---
+++ test/src/charmap.asm
@@ -0,0 +1,10 @@
+NEWCHARMAP cmap
+CHARMAP "<up>", 1
+CHARMAP "<down>", 2
+CHARMAP "<right>", 3
+CHARMAP "<left>", 4
+CHARMAP "<smiley>", $3a, $29
+CHARMAP "<supports save states>", $16, $17, $18, $19, $1a, $1b, $1c, $1d, $1e, $1f
+CHARMAP " ", $20
+CHARMAP "a", $41
+CHARMAP "b", $42
