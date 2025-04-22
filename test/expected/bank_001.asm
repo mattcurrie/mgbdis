236b857af9bc35537f3f2d419089279c87ee6c9f
Matt Currie <me@mattcurrie.com>
2025-04-22 20:57:42 +1200

    Bump version

--- test/expected/bank_001.asm
+++ test/expected/bank_001.asm
@@ -1,6 +1,6 @@
 ; Disassembly of "test.gb"
 ; This file was created with:
-; mgbdis v2.0 - Game Boy ROM disassembler by Matt Currie and contributors.
+; mgbdis v3.0 - Game Boy ROM disassembler by Matt Currie and contributors.
 ; https://github.com/mattcurrie/mgbdis
 
 SECTION "ROM Bank $001", ROMX[$4000], BANK[$1]
